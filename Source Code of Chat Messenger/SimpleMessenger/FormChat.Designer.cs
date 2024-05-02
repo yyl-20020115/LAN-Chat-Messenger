6c5d881c7b1eaf0aa66692d038ff0f42aeba02e5
unknown <yyl_20050115@hotmail.com>
2024-05-02 17:58:27 +0800

    init

---
+++ Source Code of Chat Messenger/SimpleMessenger/FormChat.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace SimpleMessenger
+{
+    partial class FormChat
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SendMsgBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.btnBuZZ = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.checkBoxEnter = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // SendMsgBox
+            // 
+            this.SendMsgBox.BackColor = System.Drawing.Color.Thistle;
+            this.SendMsgBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.SendMsgBox.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.SendMsgBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SendMsgBox.Location = new System.Drawing.Point(0, 320);
+            this.SendMsgBox.Multiline = true;
+            this.SendMsgBox.Name = "SendMsgBox";
+            this.SendMsgBox.Size = new System.Drawing.Size(280, 74);
+            this.SendMsgBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.label1.Location = new System.Drawing.Point(-3, 303);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "我发送的:";
+            // 
+            // btnSend
+            // 
+            this.btnSend.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSend.Location = new System.Drawing.Point(0, 250);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(149, 29);
+            this.btnSend.TabIndex = 3;
+            this.btnSend.Text = "发送消息(&S)";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.SendButton_Click);
+            // 
+            // btnBuZZ
+            // 
+            this.btnBuZZ.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnBuZZ.Location = new System.Drawing.Point(195, 250);
+            this.btnBuZZ.Name = "btnBuZZ";
+            this.btnBuZZ.Size = new System.Drawing.Size(73, 29);
+            this.btnBuZZ.TabIndex = 4;
+            this.btnBuZZ.Tag = "Clip";
+            this.btnBuZZ.Text = "拍拍(&P)";
+            this.btnBuZZ.UseVisualStyleBackColor = true;
+            this.btnBuZZ.Click += new System.EventHandler(this.btnBuZZ_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AllowDrop = true;
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 4);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(280, 241);
+            this.flowLayoutPanel1.TabIndex = 5;
+            this.flowLayoutPanel1.WrapContents = false;
+            // 
+            // checkBoxEnter
+            // 
+            this.checkBoxEnter.AutoSize = true;
+            this.checkBoxEnter.Checked = true;
+            this.checkBoxEnter.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBoxEnter.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.checkBoxEnter.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.checkBoxEnter.Location = new System.Drawing.Point(4, 283);
+            this.checkBoxEnter.Name = "checkBoxEnter";
+            this.checkBoxEnter.Size = new System.Drawing.Size(102, 16);
+            this.checkBoxEnter.TabIndex = 6;
+            this.checkBoxEnter.Tag = "";
+            this.checkBoxEnter.Text = "按回车发送(&E)";
+            this.checkBoxEnter.UseVisualStyleBackColor = true;
+            this.checkBoxEnter.CheckedChanged += new System.EventHandler(this.CheckBoxEnter_CheckedChanged);
+            // 
+            // FormChat
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.Purple;
+            this.ClientSize = new System.Drawing.Size(280, 394);
+            this.Controls.Add(this.checkBoxEnter);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.btnBuZZ);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SendMsgBox);
+            this.MaximizeBox = false;
+            this.Name = "FormChat";
+            this.Text = "聊天窗口";
+            this.Load += new System.EventHandler(this.FormChat_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox SendMsgBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Button btnBuZZ;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.CheckBox checkBoxEnter;
+    }
+}
---
+++ Source Code of Chat Messenger/SimpleMessenger/MessengerClient.cs
@@ -0,0 +1,238 @@
+﻿using System.Collections.Generic;
+using System.Timers;
+
+namespace SimpleMessenger;
+
+public delegate void SERVER_CONNECTION_DELIGATE(string serverIP, bool success);
+public delegate void SERVER_JOIN(ClientInfo info);
+public delegate void SERVER_LEAVE(ClientInfo info);
+public delegate void SERVER_MSG(ClientInfo info, string msg, int remoteID, int line);
+public delegate void SERVER_STATUS(ClientInfo info, string sts, ClientMsgType type);
+public delegate void SERVER_List(List<ClientInfo> l);
+public delegate void SERVER_BUZZ(int senderID);
+public delegate void SERVER_DISCONNECTED_BY_SERVER();
+
+/// <summary>
+///  ************************** This Class is  For CLIENT****************************
+///  ****Here Strat client's SocketListening thread, Message Analysis happens,and hold other neccesary info for client*********.
+/// </summary>
+
+
+public class MessengerClient
+{
+    
+    /// <summary>
+    /// Inroducing custom Events
+    /// </summary>
+    /// 
+    public event SERVER_CONNECTION_DELIGATE ConnectionStatus;
+    public event SERVER_LEAVE ClientLeaved;
+    public event SERVER_MSG NewMsg;
+    public event SERVER_STATUS NewStatus;
+    public event SERVER_List NewList;
+    public event SERVER_BUZZ NewBuzz;
+    public event SERVER_DISCONNECTED_BY_SERVER DisconnectByServer;
+    
+
+    private Dictionary<int, ClientInfo> clientDic;
+    public int[] numberoOfMessage = new int[1000];
+    public string[] numberoOfMessageString = new string[1000];
+    public bool messgSound = true;
+    private ClientInfo me;
+    private SocketListener listener;
+    public string serverIP;
+    public string ownIP;
+    readonly Timer timer = new(3000);
+    readonly Timer timerForAlive = new(3000);
+
+
+
+
+    /// <summary>
+    /// Sharing Dictionary instance with other class
+    /// </summary>
+    public Dictionary<int, ClientInfo> ClientDic => clientDic;
+
+
+    /// <summary>
+    /// Shareing SocketListener instance with other class.
+    /// </summary>
+    public SocketListener Listener => listener;
+
+
+    /// <summary>
+    /// Defalt Constructor.
+    /// </summary>
+    public MessengerClient()
+    {
+
+    }
+
+    
+    /// <summary>
+    /// Start listening in a port, send joining message to server,
+    /// </summary>
+    /// <param name="serverIP"></param>
+    /// <param name="name"></param>
+    public void Start(string serverIP,string name)
+    {
+        this.serverIP = serverIP;
+        me = new ClientInfo
+        {
+            IP = Program.OwnIP,
+            Name = name
+        };
+
+        clientDic = [];
+        listener = new SocketListener(0, GotClientMessage);
+        Program.App.Info.ListenPort = listener.Port;
+        me.ListenPort = listener.Port;
+        // Sending joining message to server 
+        ClientMessage msg = new()
+        {
+            Info = me,
+            Type = (int)ClientMsgType.Join
+        };
+        listener.Send(serverIP,12345,msg.Serialize());
+        timer.Elapsed += new ElapsedEventHandler(Timer_Elapsed);
+        timer.Start();
+        timerForAlive.Elapsed += new ElapsedEventHandler(TimerForAlive_Elapsed);
+        timerForAlive.Start();
+    }
+
+
+
+
+    /// <summary>
+    /// It will send a alive message within 3 seconds.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    void TimerForAlive_Elapsed(object sender, ElapsedEventArgs e)
+    {
+        timerForAlive.Stop();
+        //throw new NotImplementedException();
+        ClientMessage m = new ClientMessage();
+        m.Type = (int)ClientMsgType.Alive;
+        m.Info = Program.App.Info;
+        listener.Send(Program.App.ServerIP,12345,m.Serialize());
+        timerForAlive.Start();
+    }
+
+
+
+    /// <summary>
+    /// if client become unable to connect with server within 3 seconds.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    void Timer_Elapsed(object sender, ElapsedEventArgs e)
+    {
+        ConnectionStatus(serverIP, false);
+        timer.Stop();
+    }
+
+
+
+    /// <summary>
+    /// When Client get any message. Here Message type will be checked. 
+    /// </summary>
+    /// <param name="data"></param>
+    /// <param name="dataAvailable"></param>
+    private void GotClientMessage(byte[] data, int dataAvailable)
+    {
+
+        var msg = ClientMessage.DeSerialize(data, 0, dataAvailable);
+
+        //Checking Which Type of Message Client got.
+        switch ((ClientMsgType)msg.Type)
+        {
+
+            case ClientMsgType.ClientList:
+
+                timer.Stop();
+                Program.App.Info.ClientID = msg.Info.ClientID;        
+                foreach (ClientInfo info in msg.CurrentClients)
+                {
+                    if (clientDic.ContainsKey(info.ClientID) == false)
+                        clientDic.Add(info.ClientID, info);
+                }
+                if (ConnectionStatus!=null)
+                ConnectionStatus(serverIP, true);
+                break;
+
+
+
+            case ClientMsgType.clientListForALL:
+
+                msg.CurrentClients.Add(msg.Info);
+                foreach (ClientInfo info in msg.CurrentClients)
+                {
+                    if (clientDic.ContainsKey(info.ClientID) == false)
+                        clientDic.Add(info.ClientID, info);
+                }
+                if(NewList!=null)
+                NewList(msg.CurrentClients);
+                if (NewStatus != null)
+                    NewStatus(msg.Info, msg.Status, ClientMsgType.Join);
+                break;
+           
+
+
+            case ClientMsgType.Msg:
+
+                if(NewMsg!=null) 
+                    NewMsg(msg.Info, msg.Msg, msg.From,msg.LineNumb);
+                break;
+
+
+
+            case ClientMsgType.Disconnect:
+                if (NewStatus != null)
+                    NewStatus(msg.Info, msg.Status, ClientMsgType.Disconnect);
+                if (clientDic.ContainsKey(msg.Info.ClientID) == true)
+                    clientDic.Remove(msg.Info.ClientID);
+                if (ClientLeaved!=null)
+                ClientLeaved(msg.Info);
+                break;
+
+            case ClientMsgType.Status:
+
+                if(NewStatus!=null)
+                    NewStatus(msg.Info,msg.Status, ClientMsgType.Status);
+                break;
+
+            case ClientMsgType.Buzz:
+
+                if (NewBuzz != null)
+                    NewBuzz(msg.From);
+                break;
+
+            case ClientMsgType.disconnectedByServer:
+                if (DisconnectByServer!=null)
+                DisconnectByServer();
+                break;
+        }
+    }
+
+
+
+    /// <summary>
+    /// This will stop all client thread and dispose.
+    /// </summary>
+    public void Dispose()
+    {
+        listener.RunServer = false;
+
+        foreach (var f in Program.App.Forms.Values)
+        {
+            f.Close();
+            f.Dispose();
+
+        }
+        Program.App.Forms.Clear();
+        Program.App.Client.clientDic.Clear();
+    }
+}
+
+
